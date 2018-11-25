0fce8b8697b8d68b9f8d5498842392ad57b79deb
Saurabh Lende <saurabhlende@gmail.com>
2018-11-25 17:12:23 +0530

    Fetching users from database

--- Users/Users.js
+++ Users/Users.js
@@ -1,9 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
 
-router.get('/', (req, res) => {
-	console.log("Inside Users routes");
-	res.json({result: "Users routes"})
+mongoose.connect('mongodb://localhost:27017/Vysly', (err, db) => {
+	if(err){
+		console.log(err);
+	}else{
+		console.log('connected to mongodb');
+	}
+});
+
+const Schema = mongoose.Schema;
+
+const UsersSchema = new Schema({}, {strict: false});
+const Users = mongoose.model('Users', UsersSchema, 'users');
+
+router.get('/get', (req, res) => {
+	Users.find({})
+	.exec((err, users) => {
+		if(err){
+			console.log(err);
+			res.json(err);
+		}else {
+			console.log('users data found');
+			res.json({data: users});
+		}
+	})
 });
 
 module.exports = router;
